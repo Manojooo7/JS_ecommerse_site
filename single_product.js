993c6e24e41f49452c4906986940438a69df1925
Manojooo7 <manojmondal9834@gmail.com>
2023-10-27 23:37:58 +0530

    sp_core_function_implemented

--- single_product.js
+++ single_product.js
@@ -1,60 +1,40 @@
-// // Function to populate product details
-// function populateProductDetails() {
-//     // Extract the product ID from the URL parameters
-//     const urlParams = new URLSearchParams(window.location.search);
-//     const productId = urlParams.get("product");
-//     console.log(productId);
+// Function to populate product details
+function populateProductDetails() {
+    // Extract the product ID from the URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get("product");
+    console.log(productId);
 
-//     // Find the product with the matching ID from your data source (e.g., sneakers array)
-//     // const sneakersID = sneakers.map((sneaker) => sneaker.id);
-//     const product = sneakers.find((sneaker) => sneaker.id === productId);
-//     console.log(product);
+    // Find the product with the matching ID from your data source (e.g., sneakers array)
+    // const sneakersID = sneakers.map((sneaker) => sneaker.id);
+    // convert the sneaker.id id to a string then compare it with productId
 
-//     // Check if a product was found
-//     // product returns undefined ecen if the product id is correct explain why
-
-
-//     if (product) {
-//         // Populate the product details on the page
-//         const productImageElement = document.getElementById("productImage");
-//         const productTitleElement = document.getElementById("productTitle");
-//         const productDescriptionElement = document.getElementById("productDescription");
-
-//         // Set the product image, title, and description
-//         productImageElement.innerHTML = `<img src="${product.img}" alt="${product.name}" />`;
-//         productTitleElement.textContent = product.name;
-//         productDescriptionElement.textContent = product.description;
-
-//         // Add more code to populate other details as needed
-//     } else {
-//         // Handle the case where the product with the specified ID was not found
-//         console.log("Product not found");
-//     }
-// }
-
-// // Call the function to populate product details when the page loads
-// window.addEventListener("load", populateProductDetails);
-
-const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get("product");
-console.log(productId);
-const product = findProductById(productId);
-
-function findProductById(productId) {
-    for (let i = 0; i < sneakers.length; i++) {
-        if (sneakers[i].id === productId) {
-            return sneakers[i]; // Found a matching product
-        }
-    }
-    return null; // Product not found
-}
-
-if (product) {
-    // Product found, you can use 'product' here
+    const product = sneakers.find((sneaker) => sneaker.id.toString() === productId);
     console.log(product);
-} else {
-    // Handle the case where the product with the specified ID was not found
-    console.log("Product not found");
+
+    // Check if a product was found
+    // product returns undefined ecen if the product id is correct explain why
+
+
+    if (product) {
+        // Populate the product details on the page
+        const productImageElement = document.getElementById("productImage");
+        const productTitleElement = document.getElementById("productTitle");
+        const productDescriptionElement = document.getElementById("productDescription");
+        const pageTitle = document.getElementById("pageTitle");
+        console.log(pageTitle);
+        // Set the product image, title, and description
+        productImageElement.innerHTML = `<img src="${product.img}" alt="${product.name}" />`;
+        console.log(productImageElement);
+        productTitleElement.textContent = product.name;
+        productDescriptionElement.textContent = product.description;
+        pageTitle.textContent = product.name;
+        // Add more code to populate other details as needed
+    } else {
+        // Handle the case where the product with the specified ID was not found
+        console.log("Product not found");
+    }
 }
 
-window.addEventListener("load", findProductById);
+// Call the function to populate product details when the page loads
+window.addEventListener("load", populateProductDetails);
