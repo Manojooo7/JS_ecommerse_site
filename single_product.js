a1917e9d9a5be4d0e4ca513ff2c16386253e6619
Manojoo7 <manojmondl9834@gmail.com>
2023-10-28 12:02:08 +0530

    getiing all product details from local product

--- single_product.js
+++ single_product.js
@@ -1,34 +1,51 @@
+// Populate the product details on the page
+const productImageElement = document.getElementById("productImage");
+const productTitleElement = document.getElementById("productTitle");
+const productDescriptionElement = document.getElementById("productDescription");
+const pageTitle = document.getElementById("pageTitle");
+const productPrice = document.querySelector(".product_price");
+const sizes = document.querySelector(".sizes_container");
+const colors = document.querySelector(".color_container");
 // Function to populate product details
 function populateProductDetails() {
     // Extract the product ID from the URL parameters
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get("product");
-    console.log(productId);
 
     // Find the product with the matching ID from your data source (e.g., sneakers array)
     // const sneakersID = sneakers.map((sneaker) => sneaker.id);
     // convert the sneaker.id id to a string then compare it with productId
 
     const product = sneakers.find((sneaker) => sneaker.id.toString() === productId);
-    console.log(product);
 
     // Check if a product was found
     // product returns undefined ecen if the product id is correct explain why
-
-
     if (product) {
-        // Populate the product details on the page
-        const productImageElement = document.getElementById("productImage");
-        const productTitleElement = document.getElementById("productTitle");
-        const productDescriptionElement = document.getElementById("productDescription");
-        const pageTitle = document.getElementById("pageTitle");
-        console.log(pageTitle);
         // Set the product image, title, and description
-        productImageElement.innerHTML = `<img src="${product.img}" alt="${product.name}" />`;
-        console.log(productImageElement);
+        productImageElement.innerHTML = `<img src="${product.productImg}" alt="${product.name}" />`;
         productTitleElement.textContent = product.name;
         productDescriptionElement.textContent = product.description;
         pageTitle.textContent = product.name;
+        productPrice.textContent = `₹${product.price}`;
+
+        // Loop through the sizes array and create a span element for each size
+        for (let i = 0; i < product.sizes.length; i++) {
+            const size = document.createElement("span");
+            size.classList.add("sizes");
+            size.textContent = product.sizes[i];
+            sizes.appendChild(size);
+        }
+        // Loop through the colors array and create a span element for each color
+        for (let i = 0; i < product.color.length; i++) {
+            const color = document.createElement("span");
+            color.classList.add("colors");
+            const gaveColor = document.querySelector(".colors");
+            gaveColor.style.backgroundColor = product.color[i];
+            console.log(product.color[i]);
+            // console.log(color);
+            colors.appendChild(color);
+
+        }
         // Add more code to populate other details as needed
     } else {
         // Handle the case where the product with the specified ID was not found
--- sneakers.js
+++ sneakers.js
@@ -5,8 +5,9 @@ const sneakers = [
         price: 4000,
         description: 'Introducing the Nike Air Max 720 Black Edition 2023 - the pinnacle of style, innovation, and comfort in the world of athletic footwear. Elevate your sneaker game to new heights with these cutting-edge kicks that blend iconic design with the latest technology for an unparalleled walking and running experience.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'color',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes1.png?raw=true'
+        color: ['black', 'blue'],
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes1.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 2,
@@ -14,8 +15,9 @@ const sneakers = [
         price: 4000,
         description: 'Super cool and modern sneaks from PUMA in the RS-X Reinvent model. Made with a lightly chunky heel, which gives a nice look and is also made with their midsole cushioning technology which makes them flexible and moving and comfortable for your foot to wear for a whole day. Now simply choose what colour combination you want.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'pink',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes2.png?raw=true'
+        color: ['pink', 'balck'],
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes2.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1608666634759-4376010f863d?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 3,
@@ -23,8 +25,9 @@ const sneakers = [
         price: 6795,
         description: 'Introducing the NIKE SuperRep Go Training & Gym Shoes For Men – the ultimate choice for those who demand superior performance and unmatched style in their athletic footwear. Designed to help you conquer your most rigorous training sessions and workouts, these shoes are engineered to empower your every move.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'green',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes3.png?raw=true'
+        color: ['green', 'black'],
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes3.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 4,
@@ -32,8 +35,9 @@ const sneakers = [
         price: 2569,
         description: 'NIKE Legend Essential 2 Training & Gym Shoes For Men in sleek and versatile Black – the perfect footwear choice for those who seek a balance of style and performance in their workouts. With their exceptional design and innovative features, these shoes are tailored to empower your fitness journey.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'black',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes4.png?raw=true'
+        color: ['black', 'blue'],
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes4.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1605408499391-6368c628ef42?auto=format&fit=crop&q=80&w=1920&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 5,
@@ -41,8 +45,9 @@ const sneakers = [
         price: 3499,
         description: 'Elevate your basketball game with the Nike Kyrie 7 TB Black White Swoosh, a dynamic and stylish addition to the Kyrie Irving signature series. Designed for both performance and aesthetic appeal, these basketball shoes are built to help you excel on the court while making a bold statement with their classic black and white color scheme.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'black',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes5.png?raw=true'
+        color: ['black', 'yellow'],
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes5.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1605348532760-6753d2c43329?auto=format&fit=crop&q=80&w=1920&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 6,
@@ -50,8 +55,9 @@ const sneakers = [
         price: 3499,
         description: 'Elevate your basketball game with the Nike Kyrie 7 TB Black White Swoosh, a dynamic and stylish addition to the Kyrie Irving signature series. Designed for both performance and aesthetic appeal, these basketball shoes are built to help you excel on the court while making a bold statement with their classic black and white color scheme.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'orange',
-        img: './shoes6.png'
+        color: ['orange', 'black'],
+        img: './shoes6.png',
+        productImg: 'https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?auto=format&fit=crop&q=80&w=1925&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 7,
@@ -59,8 +65,9 @@ const sneakers = [
         price: 3499,
         description: 'Elevate your basketball game with the Nike Kyrie 7 TB Black White Swoosh, a dynamic and stylish addition to the Kyrie Irving signature series. Designed for both performance and aesthetic appeal, these basketball shoes are built to help you excel on the court while making a bold statement with their classic black and white color scheme.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'orange',
-        img: './shoes7.png'
+        color: ['orange', 'black'],
+        img: './shoes7.png',
+        productImg: 'https://images.unsplash.com/photo-1600185365778-7875a359b924?auto=format&fit=crop&q=80&w=1925&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 8,
@@ -68,8 +75,9 @@ const sneakers = [
         price: 1019,
         description: 'Elevate your game and your style with the Nike 130690-601 Air Jordan 12 Basketball Shoes for Men in a striking Red colorway. These iconic sneakers, part of the legendary Air Jordan series, not only offer top-tier performance on the court but also make a bold fashion statement.',
         sizes: [5, 6, 7, 8, 9],
-        color: 'orange',
-        img: 'https://raw.githubusercontent.com/Manojooo7/JS_ecommerse_site/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes10.png'
+        color: ['orange', 'black'],
+        img: 'https://raw.githubusercontent.com/Manojooo7/JS_ecommerse_site/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes10.png',
+        productImg: 'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
 
     {
@@ -79,7 +87,8 @@ const sneakers = [
         description: 'NIKE Legend Essential 2 Training & Gym Shoes For Men in sleek and versatile Black – the perfect footwear choice for those who seek a balance of style and performance in their workouts. With their exceptional design and innovative features, these shoes are tailored to empower your fitness journey.',
         sizes: [5, 6, 7, 8, 9],
         color: 'black',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes4.png?raw=true'
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes4.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1605408499391-6368c628ef42?auto=format&fit=crop&q=80&w=1920&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 21,
@@ -88,7 +97,8 @@ const sneakers = [
         description: 'Introducing the Nike Air Max 720 Black Edition 2023 - the pinnacle of style, innovation, and comfort in the world of athletic footwear. Elevate your sneaker game to new heights with these cutting-edge kicks that blend iconic design with the latest technology for an unparalleled walking and running experience.',
         sizes: [5, 6, 7, 8, 9],
         color: 'color',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes1.png?raw=true'
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes1.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 25,
@@ -97,7 +107,8 @@ const sneakers = [
         description: 'Super cool and modern sneaks from PUMA in the RS-X Reinvent model. Made with a lightly chunky heel, which gives a nice look and is also made with their midsole cushioning technology which makes them flexible and moving and comfortable for your foot to wear for a whole day. Now simply choose what colour combination you want.',
         sizes: [5, 6, 7, 8, 9],
         color: 'pink',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes2.png?raw=true'
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes2.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1608666634759-4376010f863d?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 33,
@@ -106,7 +117,8 @@ const sneakers = [
         description: 'Introducing the NIKE SuperRep Go Training & Gym Shoes For Men – the ultimate choice for those who demand superior performance and unmatched style in their athletic footwear. Designed to help you conquer your most rigorous training sessions and workouts, these shoes are engineered to empower your every move.',
         sizes: [5, 6, 7, 8, 9],
         color: 'green',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes3.png?raw=true'
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes3.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 51,
@@ -115,7 +127,8 @@ const sneakers = [
         description: 'Elevate your basketball game with the Nike Kyrie 7 TB Black White Swoosh, a dynamic and stylish addition to the Kyrie Irving signature series. Designed for both performance and aesthetic appeal, these basketball shoes are built to help you excel on the court while making a bold statement with their classic black and white color scheme.',
         sizes: [5, 6, 7, 8, 9],
         color: 'black',
-        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes5.png?raw=true'
+        img: 'https://github.com/Manojooo7/JS_ecommerse_site/blob/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes5.png?raw=true',
+        productImg: 'https://images.unsplash.com/photo-1605348532760-6753d2c43329?auto=format&fit=crop&q=80&w=1920&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 36,
@@ -124,7 +137,8 @@ const sneakers = [
         description: 'Elevate your basketball game with the Nike Kyrie 7 TB Black White Swoosh, a dynamic and stylish addition to the Kyrie Irving signature series. Designed for both performance and aesthetic appeal, these basketball shoes are built to help you excel on the court while making a bold statement with their classic black and white color scheme.',
         sizes: [5, 6, 7, 8, 9],
         color: 'orange',
-        img: './shoes6.png'
+        img: './shoes6.png',
+        productImg: 'https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?auto=format&fit=crop&q=80&w=1925&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 72,
@@ -133,7 +147,8 @@ const sneakers = [
         description: 'Elevate your basketball game with the Nike Kyrie 7 TB Black White Swoosh, a dynamic and stylish addition to the Kyrie Irving signature series. Designed for both performance and aesthetic appeal, these basketball shoes are built to help you excel on the court while making a bold statement with their classic black and white color scheme.',
         sizes: [5, 6, 7, 8, 9],
         color: 'orange',
-        img: './shoes7.png'
+        img: './shoes7.png',
+        productImg: 'https://images.unsplash.com/photo-1600185365778-7875a359b924?auto=format&fit=crop&q=80&w=1925&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
     {
         id: 48,
@@ -142,6 +157,7 @@ const sneakers = [
         description: 'Elevate your game and your style with the Nike 130690-601 Air Jordan 12 Basketball Shoes for Men in a striking Red colorway. These iconic sneakers, part of the legendary Air Jordan series, not only offer top-tier performance on the court but also make a bold fashion statement.',
         sizes: [5, 6, 7, 8, 9],
         color: 'orange',
-        img: 'https://raw.githubusercontent.com/Manojooo7/JS_ecommerse_site/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes10.png'
+        img: 'https://raw.githubusercontent.com/Manojooo7/JS_ecommerse_site/b99b51450210ac4c9dca809bb99e025d9b57e6d2/shoes10.png',
+        productImg: 'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
     },
 ]
